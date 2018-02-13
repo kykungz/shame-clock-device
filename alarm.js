3cac071d98d029b8bb048599f6f1bea2ec6c24af
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2018-02-13 21:42:00 +0700

    :bug: Fix schedule error

--- alarm.js
+++ alarm.js
@@ -8,7 +8,7 @@ class Alarm {
       const timer = setTimeout(() => {
         console.log('alarrrrrm')
       }, date - new Date())
-      this.schedule.push({ date, timer })
+      this.schedule.push({ date, timer: timer })
       return { date }
     } catch (err) {
       throw err
