5c457eed72b799d89c355cb56e4f2a2ad450707a
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2018-02-14 18:26:04 +0700

    :sparkles: Add cors

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const cors = require('cors');
 
 var index = require('./routes/index');
 
 var app = express();
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
